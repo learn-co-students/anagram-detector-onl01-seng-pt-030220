352d3a6c3613cd32fa0cdbb1784b5aeb2da32e50
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-16 23:45:06 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(words)
+    anagram = [ ]
+    words.each do |word|
+      if word.chars.sort == @word.chars.sort
+        anagram << word
+      end 
+    end
+    return anagram
+  end
+  
+end 
