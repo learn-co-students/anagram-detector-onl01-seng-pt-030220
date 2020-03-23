fadf6458c3cfd6d241e52b3853c43d284b553c25
Clannis <Dodds1990@gmail.com>
2020-03-23 00:06:38 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        return_array = []
+        #test if @word contains the same letters as any word in the passed in array
+        array.each do |test_word|
+            if test_word.split("").sort == @word.split("").sort
+                return_array << test_word
+            end
+        end
+        return_array
+    end
+
+
+
+end
