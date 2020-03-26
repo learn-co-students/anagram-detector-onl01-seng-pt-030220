724b6712ba9946b1b4644f4937cd59158799e8a7
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-03-25 22:25:54 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :anagram
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(anagram_phrase)
+    # anagram_phrase.select{ |words| words.split("").sort == @anagram.split("").sort }
+    anagram_phrase.keep_if{ |words| words.split("").sort == @anagram.split("").sort }
+  end
+
+
+end
+
+listen = Anagram.new("treehouse")
+puts listen.match(%w(housetree google inlets banana seethroue))
