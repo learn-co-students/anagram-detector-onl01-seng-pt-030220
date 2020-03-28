950001d28edca81044725dbd272d2842bd2e9f94
Christopher Cid <iamcid17@gmail.com>
2020-03-28 01:02:24 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :anagram
+  
+  def initialize(anagram)
+    @anagram = anagram
+  end
+  
+  def match(anagram_array)
+    anagram_array.select do |words|
+      (@anagram.split("").sort == words.split("").sort)
+    end
+  end
+end
