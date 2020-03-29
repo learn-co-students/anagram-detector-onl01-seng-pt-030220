c498badb346ffadac7b556976036563f820e564f
M. Gage Morgan <gage@gages.blog>
2020-03-29 18:33:44 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    detected_anagrams = []
+    # words = %w(hello world zombies pants dipper)
+    original_word = @word.split("")
+    for letters in words
+      word = letters.split("")
+      sorted_word = word.sort
+      sorted_original_word = original_word.sort
+
+      if sorted_word == sorted_original_word
+        detected_anagrams << letters
+      end
+      #binding.pry
+    end
+    detected_anagrams
+  end
+end
