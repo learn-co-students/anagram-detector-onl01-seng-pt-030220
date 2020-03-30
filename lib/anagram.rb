6fc8951d52c0156cda9dbb9bc0acceca77e05ae6
Dexter Fitch <fitch.dexter@gmail.com>
2020-03-30 21:51:23 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(list)
+    matches = []
+    list.each do |list_word|
+      if list_word.chars.sort == @word.chars.sort
+        matches << list_word
+      end
+    end
+    return matches
+  end
+end
