0ca07583f7d69fd97dd5f7c3b1ffcefc9bacc031
Craig Storer <craigstorer@gmail.com>
2020-03-31 21:11:47 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initalize(word)
+    @word = word 
+end
+
+def match(words)
+  
+  words.select {|x| x.split("").sort == @word.split("").sort}
+end
+  
+end
